7dfea294292128175341dba7528086770a3dec77
justatry7 <68475325+justatry7@users.noreply.github.com>
2025-02-09 00:07:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,84 +3,73 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"strings"
 
-	"github.com/go-telegram-bot-api/telegram-bot-api"
-)
-
-var bot *tgbotapi.BotAPI
-
-const (
-	// Токен для Telegram бота
-	TOKEN = "YOUR_BOT_TOKEN"
+	"github.com/joho/godotenv"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/axios/axios" // подключение для скачивания видео
 )
 
 func main() {
-	var err error
+	// Загружаем переменные окружения из .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Получаем BOT_TOKEN из переменных окружения
+	botToken := os.Getenv("BOT_TOKEN")
+	if botToken == "" {
+		log.Fatal("BOT_TOKEN is not set in the .env file")
+	}
+
 	// Инициализация бота
-	bot, err = tgbotapi.NewBotAPI(TOKEN)
+	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Bot authorized with username", bot.Self.UserName)
 
-	// Устанавливаем режим работы
-	bot.Debug = true
-
-	// Получаем обновления от бота
+	// Создаем обновления
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 	updates, err := bot.GetUpdatesChan(u)
 
-	// Обработка команд
+	// Обрабатываем команды и сообщения
 	for update := range updates {
-		if update.Message == nil {
+		if update.Message == nil { // Игнорируем не сообщения
 			continue
 		}
 
-		// Обработчик команды /start
+		// Команда /start
 		if update.Message.Text == "/start" {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Send me a Facebook video link, and I'll download it for you!")
 			bot.Send(msg)
+		}
+
+		// Проверка на ссылку
+		if strings.Contains(update.Message.Text, "facebook.com") {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Downloading video, please wait...")
+			bot.Send(msg)
+
+			// Логика скачивания видео
+			downloadVideo(update.Message.Text)
+
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Here is your video!")
+			bot.Send(msg)
 		} else {
-			// Проверка, является ли ссылка на Facebook
-			if isFacebookLink(update.Message.Text) {
-				// Сообщение о начале загрузки
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Downloading video, please wait...")
-				bot.Send(msg)
-
-				// Скачивание видео
-				videoURL, err := downloadFacebookVideo(update.Message.Text)
-				if err != nil {
-					errorMsg := tgbotapi.NewMessage(update.Message.Chat.ID, "An error occurred while downloading the video.")
-					bot.Send(errorMsg)
-					continue
-				}
-
-				// Отправка видео
-				videoMsg := tgbotapi.NewVideo(update.Message.Chat.ID, videoURL)
-				bot.Send(videoMsg)
-			} else {
-				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "This is not a Facebook video link!")
-				bot.Send(msg)
-			}
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "This is not a Facebook video link!")
+			bot.Send(msg)
 		}
 	}
 }
 
-// Проверка, является ли ссылка на Facebook
-func isFacebookLink(url string) bool {
-	return strings.Contains(url, "facebook.com")
-}
-
-// Функция для скачивания видео с Facebook (реальная логика будет зависеть от API или метода загрузки)
-func downloadFacebookVideo(url string) (string, error) {
-	// Вставь сюда логику для скачивания видео
-	// Пример заглушки: просто возвращаем ссылку на видео
-	// Реальную логику можно реализовать, используя API или парсинг страницы.
-
-	// Пример ссылки на скачанное видео
-	videoDownloadURL := "https://example.com/path/to/facebook/video.mp4"
-	return videoDownloadURL, nil
+// Функция скачивания видео
+func downloadVideo(url string) {
+	// Код для скачивания видео с Facebook через axios
+	_, err := axios.Get("https://api.fdown.net/api/download?url=" + url)
+	if err != nil {
+		log.Fatal("Error downloading video: ", err)
+	}
 }
